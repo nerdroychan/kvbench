47295653dfacfeed565bd0e77d08983c39d36c8e
Chen Chen <mail@roychan.org>
2024-07-28 20:29:00 -0500

    examples: update examples with new output format

--- examples/mixed/run.sh
+++ examples/mixed/run.sh
@@ -2,7 +2,7 @@
 
 DIR="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
-cargo build --profile release-lto --all-features
+cargo +stable build --profile release-lto --all-features
 
 STORE_DIR=$DIR/../../presets/stores
 BENCHMARK=$DIR/mixed.toml
@@ -10,5 +10,11 @@ BENCHMARK=$DIR/mixed.toml
 STORES="chashmap contrie dashmap flurry papaya scchashmap mutex_hashmap rwlock_hashmap"
 
 for s in $STORES; do
-    env global.threads=32 cargo run --profile release-lto --all-features -- bench -s $STORE_DIR/$s.toml -b $BENCHMARK 2>/dev/null | tee $s.txt
+    echo $s
+    rm $s.txt 2>/dev/null
+    env global.threads=32 \
+        cargo +stable run --profile release-lto --all-features -- \
+        bench -s $STORE_DIR/$s.toml -b $BENCHMARK 2>/dev/null | tee $s.txt
 done
+
+gnuplot $DIR/plot.gpl
--- examples/writeheavy/run.sh
+++ examples/writeheavy/run.sh
@@ -2,7 +2,7 @@
 
 DIR="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
-cargo build --profile release-lto --all-features
+cargo +stable build --profile release-lto --all-features
 
 STORE_DIR=$DIR/../../presets/stores
 BENCHMARK=$DIR/writeheavy.toml
@@ -10,7 +10,12 @@ BENCHMARK=$DIR/writeheavy.toml
 STORES="chashmap contrie dashmap flurry papaya scchashmap mutex_hashmap rwlock_hashmap"
 
 for s in $STORES; do
+    echo $s
+    rm $s.txt 2>/dev/null
     for t in `seq 1 16`; do
-        env global.threads=$t cargo run --profile release-lto --all-features -- bench -s $STORE_DIR/$s.toml -b $BENCHMARK 2>/dev/null | tee -a $s.txt
+        data="$(env global.threads=$t cargo +stable run --profile release-lto --all-features -- bench -s $STORE_DIR/$s.toml -b $BENCHMARK 2>/dev/null)"
+        echo "threads $t $data" | tee -a $s.txt
     done
 done
+
+gnuplot $DIR/plot.gpl
